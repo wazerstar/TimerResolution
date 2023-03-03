ca4a0724488eecefab459afb6b687b2a404b2c80
amitxv <86924251+amitxv@users.noreply.github.com>
2023-03-03 18:35:48 +0000

    Dynamically load SetProcessInformation + rework

--- MeasureSleep.cpp
+++ MeasureSleep.cpp
@@ -1,10 +1,11 @@
+#include <iomanip>
 #include <iostream>
 #include <windows.h>
 
 typedef NTSTATUS(CALLBACK *NTQUERYTIMERRESOLUTION)(
     OUT PULONG MinimumResolution,
     OUT PULONG MaximumResolution,
-    OUT PULONG CurrentResolutionolution);
+    OUT PULONG CurrentResolution);
 
 int main() {
     ULONG MinimumResolution, MaximumResolution, CurrentResolution;
@@ -12,19 +13,19 @@ int main() {
 
     QueryPerformanceFrequency(&freq);
 
-    HMODULE hNtDll = LoadLibrary(L"NtDll.dll");
+    HMODULE ntdll = LoadLibrary(L"NtDll.dll");
 
-    if (!hNtDll) {
-        printf("LoadLibrary failed");
+    if (!ntdll) {
+        std::cerr << "LoadLibrary failed\n";
         return 1;
     }
 
-    NTQUERYTIMERRESOLUTION NtQueryTimerResolution = (NTQUERYTIMERRESOLUTION)GetProcAddress(hNtDll, "NtQueryTimerResolution");
+    NTQUERYTIMERRESOLUTION NtQueryTimerResolution = (NTQUERYTIMERRESOLUTION)GetProcAddress(ntdll, "NtQueryTimerResolution");
 
     for (;;) {
         // get current resolution
-        if (NtQueryTimerResolution(&MinimumResolution, &MaximumResolution, &CurrentResolution)) {
-            printf("NtQueryTimerResolution failed");
+        if (NtQueryTimerResolution(&MinimumResolution, &MaximumResolution, &CurrentResolution) != 0) {
+            std::cerr << "NtQueryTimerResolution failed\n";
             return 1;
         }
 
@@ -36,8 +37,7 @@ int main() {
         double delta_s = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
         double delta_ms = delta_s * 1000;
 
-        printf("Resolution: %lfms, Sleep(1) slept %lfms (delta: %lf)\n",
-               CurrentResolution / 10000.0, delta_ms, delta_ms - 1);
+        std::cout << std::fixed << std::setprecision(6) << "Resolution: " << (CurrentResolution / 10000.0) << "ms, Sleep(1) slept " << delta_ms << "ms (delta: " << delta_ms - 1 << ")\n";
 
         Sleep(1000); // pause for a second between iterations
     }
--- SetTimerResolution.cpp
+++ SetTimerResolution.cpp
@@ -1,50 +1,83 @@
-#include <stdio.h>
+#include "args.hxx"
+#include <iomanip>
+#include <iostream>
 #include <windows.h>
 
 typedef NTSTATUS(CALLBACK *NTQUERYTIMERRESOLUTION)(
     OUT PULONG MinimumResolution,
     OUT PULONG MaximumResolution,
-    OUT PULONG CurrentResolutionolution);
+    OUT PULONG CurrentResolution);
 
 typedef NTSTATUS(CALLBACK *NTSETTIMERRESOLUTION)(
     IN ULONG DesiredResolution,
     IN BOOLEAN SetResolution,
-    OUT PULONG CurrentResolutionolution);
+    OUT PULONG CurrentResolution);
 
-int main() {
-    // FreeConsole(); // hides console
+typedef BOOL(WINAPI *PSET_PROCESS_INFORMATION)(HANDLE, PROCESS_INFORMATION_CLASS, LPVOID, DWORD);
+
+int main(int argc, char **argv) {
+    std::string version = "0.1.0";
+
+    args::ArgumentParser parser("SetTimerResolution " + version);
+    args::HelpFlag help(parser, "help", "display this help menu", {"help"});
+    args::ValueFlag<int> resolution(parser, "", "specify the desired resolution in 100-ns units", {"resolution"}, args::Options::Required);
+    args::Flag no_console(parser, "no-console", "hide the console window", {"no-console"});
+
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help) {
+        std::cout << parser;
+        return 0;
+    } catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    } catch (args::ValidationError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    if (no_console) {
+        FreeConsole();
+    }
 
     ULONG MinimumResolution, MaximumResolution, CurrentResolution;
-    PROCESS_POWER_THROTTLING_STATE PowerThrottling;
 
-    HMODULE hNtDll = LoadLibrary(L"NtDll.dll");
+    HMODULE ntdll = LoadLibrary(L"ntdll.dll");
+    HMODULE kernel32 = LoadLibrary(L"kernel32.dll");
 
-    if (!hNtDll) {
-        printf("LoadLibrary failed");
+    if (!ntdll or !kernel32) {
+        std::cerr << "LoadLibrary failed\n";
         return 1;
     }
 
-    NTQUERYTIMERRESOLUTION NtQueryTimerResolution = (NTQUERYTIMERRESOLUTION)GetProcAddress(hNtDll, "NtQueryTimerResolution");
-    NTSETTIMERRESOLUTION NtSetTimerResolution = (NTSETTIMERRESOLUTION)GetProcAddress(hNtDll, "NtSetTimerResolution");
+    NTQUERYTIMERRESOLUTION NtQueryTimerResolution = (NTQUERYTIMERRESOLUTION)GetProcAddress(ntdll, "NtQueryTimerResolution");
+    NTSETTIMERRESOLUTION NtSetTimerResolution = (NTSETTIMERRESOLUTION)GetProcAddress(ntdll, "NtSetTimerResolution");
+    PSET_PROCESS_INFORMATION SetProcessInformation = (PSET_PROCESS_INFORMATION)GetProcAddress(kernel32, "SetProcessInformation");
 
-    RtlZeroMemory(&PowerThrottling, sizeof(PowerThrottling));
+    // does not exist in Windows 7
+    if (SetProcessInformation) {
+        PROCESS_POWER_THROTTLING_STATE PowerThrottling;
+        RtlZeroMemory(&PowerThrottling, sizeof(PowerThrottling));
 
-    PowerThrottling.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
-    PowerThrottling.ControlMask = PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION;
-    PowerThrottling.StateMask = 0;
+        PowerThrottling.Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION;
+        PowerThrottling.ControlMask = PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION;
+        PowerThrottling.StateMask = 0;
 
-    SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling, &PowerThrottling, sizeof(PowerThrottling));
+        SetProcessInformation(GetCurrentProcess(), ProcessPowerThrottling, &PowerThrottling, sizeof(PowerThrottling));
+    }
 
     if (NtQueryTimerResolution(&MinimumResolution, &MaximumResolution, &CurrentResolution)) {
-        printf("NtQueryTimerResolution failed");
+        std::cerr << "NtQueryTimerResolution failed\n";
         return 1;
     }
 
-    if (NtSetTimerResolution(MaximumResolution, true, &CurrentResolution)) {
-        printf("NtSetTimerResolution failed");
+    if (NtSetTimerResolution(args::get(resolution), true, &CurrentResolution)) {
+        std::cerr << "NtSetTimerResolution failed\n";
         return 1;
     }
 
-    printf("Resolution set to: %lfms", (double)CurrentResolution / 10000);
+    std::cout << std::fixed << std::setprecision(6) << "Resolution set to: " << (CurrentResolution / 10000.0) << "ms\n";
     Sleep(INFINITE);
 }
